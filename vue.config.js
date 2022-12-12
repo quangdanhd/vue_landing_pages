782af98369b17dbe5ea34546bac0594a760da19e
quangdanhd <quangdanhd@gmail.com>
2022-12-12 16:50:18 +0700

    fix vue router

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,9 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: "",
+  configureWebpack: {
+    devServer: {
+      historyApiFallback: true,
+    },
+  },
 });
