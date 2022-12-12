70b565b1d6be6da44bff7ae9259341753dbb0c13
quangdanhd <quangdanhd@gmail.com>
2022-12-12 17:13:37 +0700

    vue route lazy

--- src/router.js
+++ src/router.js
@@ -1,18 +1,24 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomePage from "@/components/HomePage.vue";
-import AboutPage from "@/components/AboutPage.vue";
-import ContactPage from "@/components/ContactPage.vue";
+// import HomePage from "@/components/HomePage.vue";
+// import AboutPage from "@/components/AboutPage.vue";
+// import ContactPage from "@/components/ContactPage.vue";
+const HomePage = () => import("@/components/HomePage.vue");
+const AboutPage = () => import("@/components/AboutPage.vue");
+const ContactPage = () => import("@/components/ContactPage.vue");
 const routes = [
   {
     path: "/",
+    name: "",
     component: HomePage,
   },
   {
     path: "/about",
+    name: "",
     component: AboutPage,
   },
   {
     path: "/contact",
+    name: "",
     component: ContactPage,
   },
   {
