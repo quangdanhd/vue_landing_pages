e1a2af794b5714d683e27ccd5499f5da5b9b45d4
quangdanhd <quangdanhd@gmail.com>
2022-12-12 16:23:03 +0700

    add scss

--- src/main.js
+++ src/main.js
@@ -5,4 +5,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
+import "./assets/scss/app.scss";
+
 createApp(App).mount("#app");
