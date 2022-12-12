69fe78b4fe547d410a1de9a5aafcd9f9947533c1
quangdanhd <quangdanhd@gmail.com>
2022-12-12 17:07:03 +0700

    vue router active

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,15 +1,50 @@
 <template>
-  <div class="menu">
-    <router-link class="link" to="/">
-      <p class="item-menu">Home</p>
-    </router-link>
-    <router-link class="link" to="/about">
-      <p class="item-menu">About Us</p>
-    </router-link>
-    <router-link class="link" to="/contact">
-      <p class="item-menu">Contact</p>
-    </router-link>
-  </div>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <router-link class="navbar-brand me-1 middle" to="/">
+        <img alt="Vue logo" src="@/assets/logo.png" style="height: 25px" />
+      </router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarScroll"
+        aria-controls="navbarScroll"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarScroll">
+        <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
+          <li class="nav-item">
+            <router-link class="nav-link" active-class="active" to="/"
+              >Home</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" active-class="active" to="/about"
+              >About Us</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" active-class="active" to="/contact"
+              >Contact</router-link
+            >
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
